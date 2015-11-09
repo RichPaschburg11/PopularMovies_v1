63327f5643ac66d6dc2e82cd12f8215e7f35f3ea
RichPaschburg11 <rpaschburg@gmail.com>
2015-11-08 18:04:47 -0800

    committing project without API key
    Have gotten files from a project that incorrectly included an api key
    in one commit.
    The file ApiKey.java is in .gitignore.
    Will run
    “git -update-index —assume-unchanged
    app/src/main/com/paschburg/rich/popularmovies_v1/ApiKey.java”
    before next commit. Can modify ApiKey.java before that commit to
    contain the api key only locally.

---
+++ app/src/main/java/com/paschburg/rich/popularmovies_v1/UserPrefs.java
@@ -0,0 +1,45 @@
+package com.paschburg.rich.popularmovies_v1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by richardpaschburg on 10/10/15.
+ * Courtesy: Mobile Zone at https://dzone.com/articles/storing-objects-android
+ */
+public class UserPrefs {
+
+    private static final String packageName = "com.paschburg.rich.popularmovies.";
+    private static final String PREFS_NAME = packageName + "UserPrefs";
+    private static final String PREFS_INDEX = packageName + "PrefsIndex";
+    private static SharedPreferences settings;
+    private static SharedPreferences.Editor editor;
+
+    public UserPrefs(Context ctx){
+        if (settings == null) {
+            settings = ctx.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        }
+        editor = settings.edit();
+    }
+
+    public void setString(String index, String string) {
+        editor.putString(PREFS_INDEX + index, string);
+        editor.commit();
+    }
+
+     public String getString(String index){
+         return settings.getString(PREFS_INDEX + index, "");
+
+    }
+    public void setSortOrder(String string) {
+        editor.putString(PREFS_INDEX + "SortOrder", string);
+        editor.commit();
+        // Log.e("commmit=", string);
+    }
+
+    public String getSortOrder(){
+        String settingvalue = settings.getString(PREFS_INDEX + "SortOrder", "");
+        return settingvalue;
+
+    }
+}
